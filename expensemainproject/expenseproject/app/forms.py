df9d3e36bec2345c3d62ed06b44a12c0a0bc08e1
kiran-saju <163312392+kiran-saju@users.noreply.github.com>
2024-04-03 22:26:05 +0530

    Update forms.py

--- expensemainproject/expenseproject/app/forms.py
+++ expensemainproject/expenseproject/app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Installment
+from .models import Installment,Bill
 
 class InstallmentForm(forms.ModelForm):
     class Meta:
         model = Installment
         fields ='__all__'
+
+
+class BillForm(forms.ModelForm):
+    class Meta:
+        model = Bill
+        fields = ['installment_number', 'payment_amount', 'staff_name', 'paid_status']

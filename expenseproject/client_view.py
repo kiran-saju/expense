9bfd70f6704f86dbe55392cdbed1338197ea63b3
kiran-saju <163312392+kiran-saju@users.noreply.github.com>
2024-04-23 09:54:24 +0530

    Update client_view.py

--- expenseproject/client_view.py
+++ expenseproject/client_view.py
@@ -6,7 +6,15 @@ from django.db.models import Sum, F
 
 @login_required
 def CLIENT_HOME(request):
-    return render(request,'CLIENT/client_home.html')
+    client = request.user.client 
+    bills = Bill.objects.filter(client=client)
+    total_payment = bills.filter(paid_status=True).aggregate(total_payment=Sum('payment_amount'))['total_payment']
+    total_balance = bills.filter(paid_status=False).aggregate(total_balance=Sum('payment_amount'))['total_balance']
+    print(total_balance)
+    print(total_payment )
+    return render(request,'CLIENT/client_home.html', {'bills': bills,'total_payment':total_payment,'total_balance':total_balance})
+
+
 
 @login_required
 def client_bills(request):
